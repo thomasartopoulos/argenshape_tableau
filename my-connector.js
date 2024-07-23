5dd56573f06b8195e1b34a6a572b98fbb396e55f
Thomas Artopoulos <48850000+thomasartopoulos@users.noreply.github.com>
2024-07-23 11:29:42 -0300

    updated

--- my-connector.js
+++ my-connector.js
@@ -1,41 +1,21 @@
-(function() {
-    var myConnector = tableau.makeConnector();
-
-    myConnector.getSchema = function(schemaCallback) {
-        var cols = [
-            {id: "id", alias: "ID", dataType: tableau.dataTypeEnum.string},
-            {id: "nombre", alias: "Name", dataType: tableau.dataTypeEnum.string},
-            {id: "nombre_completo", alias: "Full Name", dataType: tableau.dataTypeEnum.string},
-            {id: "fuente", alias: "Source", dataType: tableau.dataTypeEnum.string},
-            {id: "categoria", alias: "Category", dataType: tableau.dataTypeEnum.string},
-            {id: "centroide_lon", alias: "Longitude", dataType: tableau.dataTypeEnum.float},
-            {id: "centroide_lat", alias: "Latitude", dataType: tableau.dataTypeEnum.float},
-            {id: "iso_id", alias: "ISO ID", dataType: tableau.dataTypeEnum.string},
-            {id: "iso_nombre", alias: "ISO Name", dataType: tableau.dataTypeEnum.string},
-            {id: "geometry", alias: "Geometry", dataType: tableau.dataTypeEnum.geometry}  // Add geometry data type
-        ];
-
-        var tableSchema = {
-            id: "LocationData",
-            alias: "Location Data",
-            columns: cols
-        };
-
-        console.log("Schema: ", tableSchema);
-        schemaCallback([tableSchema]);
-    };
-
-    myConnector.getData = function(table, doneCallback) {
-        function fetchData() {
-            const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-            const targetUrl = 'https://apis.datos.gob.ar/georef/api/provincias.geojson';
-            fetch(proxyUrl + targetUrl)
-                .then(response => response.json())
-                .then(data => {
+myConnector.getData = function(table, doneCallback) {
+    function fetchData() {
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const targetUrl = 'https://apis.datos.gob.ar/georef/api/provincias.geojson';
+        
+        fetch(proxyUrl + targetUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.text();  // Get response as text
+            })
+            .then(text => {
+                try {
+                    const data = JSON.parse(text);  // Parse the text as JSON
                     console.log(data);
-                    // Assuming 'data' is an array of objects, each representing a row
+
                     var tableData = [];
-                    // Process each item in the data array
                     data.features.forEach(function(feature) {
                         tableData.push({
                             "id": feature.properties.id,
@@ -47,25 +27,18 @@
                             "centroide_lat": feature.geometry.coordinates[1],
                             "iso_id": feature.properties.iso_id,
                             "iso_nombre": feature.properties.iso_nombre,
-                            "geometry": JSON.stringify(feature.geometry)  // Convert geometry object to string
+                            "geometry": JSON.stringify(feature.geometry)
                         });
                     });
 
                     table.appendRows(tableData);
                     doneCallback();
-                })
-                .catch(error => console.error('Error:', error));
-        }
+                } catch (error) {
+                    console.error('Error parsing JSON:', error);
+                }
+            })
+            .catch(error => console.error('Fetch error:', error));
+    }
 
-        fetchData();
-    };
-
-    tableau.registerConnector(myConnector);
-
-    $(document).ready(function() {
-        $("#submitButton").click(function() {
-            tableau.connectionName = "Location Data";
-            tableau.submit();
-        });
-    });
-})();
+    fetchData();
+};
